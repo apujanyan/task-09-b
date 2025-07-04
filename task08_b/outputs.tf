f74e26cca593a56a6aa5c4d316cff2edadc202a8
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-07-04 20:11:44 +0400

    ...

--- task08_b/outputs.tf
+++ task08_b/outputs.tf
@@ -7,3 +7,8 @@ output "aca_fqdn" {
   description = "ACA FQDN."
   value       = module.aca.aca_fqdn
 }
+
+output "redis_fqdn" {
+  description = "Redis FQDN."
+  value       = module.aci_redis.redis_fqdn
+}
