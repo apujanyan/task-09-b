cb373694a701bc0a2e01f62071574421e84d4d7d
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-07-05 10:33:09 +0400

    ...

--- task08_b/locals.tf
+++ task08_b/locals.tf
@@ -3,7 +3,7 @@ locals {
   keyvault_name     = join("-", [var.name_prefix, "kv"])
   aca_env_name      = join("-", [var.name_prefix, "cae"])
   aca_name          = join("-", [var.name_prefix, "ca"])
-  aks_name          = join("-", [var.name_prefix, "ks"])
+  aks_name          = join("-", [var.name_prefix, "aks"])
   redis_aci_name    = join("-", [var.name_prefix, "redis-ci"])
   docker_image_name = join("-", [var.name_prefix, "app"])
   sa_name           = replace("${var.name_prefix}sa", "-", "")
